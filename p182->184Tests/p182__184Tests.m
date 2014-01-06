85b96602fc724a0a19535e36925c5efc14c6fa38
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-06 17:28:56 +0900

    Initial Commit

---
+++ p182->184Tests/p182__184Tests.m
@@ -0,0 +1,34 @@
+//
+//  p182__184Tests.m
+//  p182->184Tests
+//
+//  Created by SDT-1 on 2014. 1. 6..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface p182__184Tests : XCTestCase
+
+@end
+
+@implementation p182__184Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
